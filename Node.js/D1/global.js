0f6294a80a0f27b14ee1fc5a0754170730477ec4
12DEEPTIKARKI <deeptik9811@gmail.com>
2023-12-09 19:31:28 +0530

    Basics

---
+++ Node.js/D1/global.js
@@ -0,0 +1,23 @@
+// The Global Object
+
+//console.log(global);
+
+// to call the specified function after a given time period only once
+
+// global.setTimeout(() => {
+//     console.log('in the timeout');
+//     // to stop the setInterval function
+//     clearInterval(int);
+// }, 3000);
+
+
+// const int = setInterval(() => {
+//     console.log('hello moto');
+// }, 1000);
+
+
+// Nodejs Properties
+//1 __dirname (Absolute path of the current file we are working on)
+console.log(__dirname);
+//2__filename ((Absolute path of the folder with file name )
+console.log(__filename);
